42124250c03e50ec0e15b4810ede6f7464b8a201
Adrian <adrianomucha@gmail.com>
2016-07-21 01:19:18 +0000

    assignment updated

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 def filter_custom(l,f):
-'''Filter a list using a function
-Return a new list that contains all the elements e of l
-    for which f(e) is True
+    '''Filter a list using a function
+    Return a new list that contains all the elements e of l
+        for which f(e) is True
 
-:param l: a list
-:param f: a function that takes one argument and returns either
-    True or False
-'''
-    pass
-def map_custom():
-'''Map a list using a function
+    :param l: a list
+    :param f: a function that takes one argument and returns either
+        True or False
+    '''
+    new_list[]
 
-Return a new list that applies f(e) for every element e in l
+    return new_list
+def map_custom(l,f):
+    '''Map a list using a function
 
-:param l: a list
-:param m: a function that takes one argument and returns a value
-'''
-    pass
-def reduce_custom():
-''' Rdeuce a list using a reducer function and a starting value
+    Return a new list that applies f(e) for every element e in l
 
-Return a single value that applies f(v, e) for every
-e in l from left to right. The initial value for v
-should be starting_value, and subsequent values should
-be the previously calculated value from f(v, e)
+    :param l: a list
+    :param m: a function that takes one argument and returns a value
+    '''
+    new_list = []
+    for e in l:
+        new_list.append(f(e))
+    return new_list
 
-:param l: a list
-:param f: a function that takes one argument and returns a value
-:param starting_value: the beggining value for the reducer function
-    computation
+def reduce_custom(l,f, starting_value):
+    ''' Rdeuce a list using a reducer function and a starting value
+
+    Return a single value that applies f(v, e) for every
+        e in l from left to right. The initial value for v
+        should be starting_value, and subsequent values should
+        be the previously calculated value from f(v, e)
+
+    :param l: a list
+    :param f: a function that takes one argument and returns a value
+    :param starting_value: the beggining value for the reducer function
+        computation
     '''
     pass
+
+if __name__ == '__main__':
+    l = [7,8,9]
+    f = lambda x: x * 10
+    print(map_custom(l,f))
 
